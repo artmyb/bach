b41d80ed7f26ea8d620d42923e26dc4b41123b84
artmyb <74744060+artmyb@users.noreply.github.com>
2023-11-27 15:10:45 +0300

    bach.py

--- bach.py
+++ bach.py
@@ -25,72 +25,93 @@ class Note:
         self.duration = duration
         self.dynamic = dynamic
         self.timbre = timbre
-        self.octave_digits = 0
-        for i in self.name:
-            try:
-                a = int(i)
-                self.octave_digits+=1
-            except:
-                pass
-        self.octave = self.name[-self.octave_digits:]
-        if len(self.timbre) < 50:
-            while len(self.timbre) < 50:
-                self.timbre.append(0)
 
     def note_set(self):
-        unique_note = self.name.replace(self.octave, "")
-        return unique_note
+        set = ""
+        for i in self.name:
+            if i.isalpha() or i == "#" or i == "b":
+                set = set + i
+        return set
+
+    def octave(self):
+        octave = self.name.replace(self.note_set(),"")
+        if "#" in octave:
+            octave = octave.replace("#","")
+        if "b" in octave:
+            octave = octave.replace("b","")
+        return int(octave)
 
     def note_index(self):
         return Note.note_names.index(self.note_set())
 
     def key(self):
-        key = self.note_index() + 12*(int(self.octave)+1)
+        key = self.note_index() + 12*(int(self.octave())+1)
         return key
 
     def frequency(self):
         return 440*2**((self.key()-69)/12)
 
-    def add(self,semitone=1):
+    def add1(self,semitone=1):
         new_note_key = self.key() + semitone
         new_note_octave = new_note_key//12 - 1
-        new_note_set = Note.note_names[self.note_index()+semitone-12]
+        new_note_set = Note.note_names[self.note_index()+semitone]
         return Note(new_note_set+str(new_note_octave),duration=self.duration,dynamic=self.dynamic,timbre=self.timbre)
+
+    def __add__(self,semitone):
+        return self.add1(semitone)
+
+    def __radd__(self,semitone):
+        return self.add1(semitone)
 
     def change_duration(self,new_duration):
         return Note(self.name, duration=new_duration, dynamic=self.dynamic,
                     timbre=self.timbre)
 
+    def __sub__(self, other):
+        if type(other) == Note:
+            return self.key() - other.key()
+        elif type(other) == int:
+            return self.add1(-other)
+
+    def __mul__(self, relative_duration):
+        new_duration = self.duration * relative_duration
+        return self.change_duration(new_duration)
+
+    def __rmul__(self, relative_duration):
+        new_duration = self.duration * relative_duration
+        return self.change_duration(new_duration)
+
     def change_dynamic(self,new_dynamic):
         return Note(self.name, duration=self.duration, dynamic=new_dynamic,
                     timbre=self.timbre)
+
     def dominant(self,which = 0):
-        return self.add(-5+which*12)
+        return self.add1(-5+which*12)
 
     def subdominant(self,which = 0):
-        return self.add(-7 + which * 12)
+        return self.add1(-7 + which * 12)
 
     def supertonic(self,which = 0):
-        return self.add(+2 + which * 12)
+        return self.add1(+2 + which * 12)
 
     def leading_tone(self):
-        return self.add(-1)
+        return self.add1(-1)
 
     def harmonic(self,n):
         key_difference = 12*np.log(n)/np.log(2)
-        return self.add(round(key_difference))
+        return self.add1(round(key_difference))
 
     def subharmonic(self,n):
         return self.harmonic(1/n)
 
-    def major_scale(self,degree = 0):
+    def major(self,degree = 0):
         steps_up = (0,2,4,5,7,9,11,12)
         steps_down = (0,1,3,5,7,8,10,12)
         if degree < 0:
-            return self.add(-steps_down[-degree//12+(-degree)%12])
-        return self.add(steps_up[degree//12+degree%12])
+            return self.add1(-steps_down[-degree//12+(-degree)%12])
+        return self.add1(steps_up[degree//12+degree%12])
 
-    def minor_scale(self,degree = 0,mode="harmonic"):
+    def minor(self,degree = 0,mode="harmonic"):
         if mode == "harmonic":
             steps_up = (0, 2, 3, 5, 7, 8, 11, 12)
             steps_down = (0, 1, 4, 5, 7, 9, 10, 12)
@@ -101,10 +122,10 @@ class Note:
             steps_up = (0, 2, 3, 5, 7, 9, 11, 12)
             steps_down = (0, 1, 3, 5, 7, 9, 10, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def ionian_scale(self,degree = 0,mode="natural"):
+    def ionian(self,degree = 0,mode="natural"):
         if mode == "harmonic":
             steps_up = (0, 2, 4, 5, 8, 9, 11, 12)
             steps_down = (0, 1, 3, 4, 7, 8, 10, 12)
@@ -115,10 +136,10 @@ class Note:
             steps_up = (0, 2, 4, 6, 8, 9, 11, 12)
             steps_down = (0, 1, 3, 4, 6, 8, 10, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def dorian_scale(self,degree = 0,mode="natural"):
+    def dorian(self,degree = 0,mode="natural"):
         if mode == "harmonic":
             steps_up = (0, 2, 3, 6, 7, 9, 10, 12)
             steps_down = (0, 2, 3, 5, 6, 9, 10, 12)
@@ -129,10 +150,10 @@ class Note:
             steps_up = (0, 2, 4, 6, 7, 9, 11, 12)
             steps_down = (0, 2, 3, 5, 6, 8, 10, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def phrygian_scale(self,degree = 0,mode="natural"):
+    def phrygian(self,degree = 0,mode="natural"):
         if mode == "harmonic":
             steps_up = (0, 1, 4, 5, 7, 8, 10, 12)
             steps_down = (0, 2, 4, 5, 7, 8, 11, 12)
@@ -143,10 +164,10 @@ class Note:
             steps_up = (0, 3, 4, 5, 7, 8, 10, 12)
             steps_down = (0, 2, 4, 5, 7, 8, 10, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def lydian_scale(self,degree = 0,mode="natural"):
+    def lydian(self,degree = 0,mode="natural"):
         if mode == "harmonic":
             steps_up = (0, 3, 4, 6, 7, 9, 11, 12)
             steps_down = (0, 1, 3, 5, 6, 8, 9, 12)
@@ -157,10 +178,10 @@ class Note:
             steps_up = (0, 2, 3, 5, 6, 8, 10, 12)
             steps_down = (0, 2, 4, 6, 7, 9, 10, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def mixolydian_scale(self,degree = 0,mode="natural"):
+    def mixolydian(self,degree = 0,mode="natural"):
         if mode == "harmonic":
             steps_up = (0, 1, 3, 4, 6, 8, 9, 12)
             steps_down = (0, 3, 4, 6, 8, 9, 11, 12)
@@ -171,10 +192,10 @@ class Note:
             steps_up = (0, 1, 3, 4, 6, 8, 10, 12)
             steps_down = (0, 2, 4, 6, 8, 9, 11, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def locrian_scale(self,degree = 0,mode="natural"):
+    def locrian(self,degree = 0,mode="natural"):
         if mode == "harmonic":
             steps_up = (0, 1, 3, 5, 6, 9, 10, 12)
             steps_down = (0, 2, 3, 6, 7, 9, 11, 12)
@@ -185,98 +206,138 @@ class Note:
             steps_up = (0, 1, 3, 5, 7, 9, 10, 12)
             steps_down = (0, 2, 3, 5, 7, 9, 11, 12)
         if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+            return self.add1(-steps_down[-degree // 12 + (-degree) % 12])
+        return self.add1(steps_up[degree // 12 + degree % 12])
 
-    def major(self,degree=0):
-        steps_up = (0, 4, 7, 12)
-        steps_down = (0, 5, 8,12)
-        if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+    def chord(self,degree=0,root=0,intervals = (0,4,7)):
+        if degree == 0:
+            return self
+        if root == 0:
+            if degree > 0:
+                notes = [self+i for i in intervals]
+                notes_add = [self+i for i in intervals]
+                for i in range(1,int(degree)):
+                    for j in notes_add:
+                        notes.append(j+i*12)
 
-    def minor(self,degree = 0):
-        steps_up = (0, 3, 7, 12)
-        steps_down = (0, 5, 9, 12)
-        if degree < 0:
-            return self.add(-steps_down[-degree // 12 + (-degree) % 12])
-        return self.add(steps_up[degree // 12 + degree % 12])
+                return notes[degree]
+            if degree < 0:
+                notes = [0]+[self-(12-i) for i in reversed(intervals)]
+                notes_add = [0]+[self-(12-i) for i in reversed(intervals)]
+                for i in range(1, int(-degree)):
+                    for j in notes_add:
+                        notes.append(j - i * 12)
+                return notes[-degree]
+        else:
+            difference = ((self+120)-Note(root+"0") )%12
+            print((self+120).name,Note(root+"0").name)
+            print(difference)
+            root_note= self-difference
+            print(root_note.name,difference,degree)
+            is_there = 0
+            for i in range(5):
+                if root_note.M(i).name == self.name:
+                    is_there += 1
+                    difference = i
+                    break
+            if is_there == 0:
+                raise Exception("Note object is not in root note's chord.")
+                return
+            return root_note.M(degree+difference)
 
-    def __sub__(self,another_note):
-        return self.key() - another_note.key()
+    def M(self, degree=0, root=0):
+        return self.chord(degree, root, (0, 4, 7))
 
-    def add_in_scale(self,add=1,tonal_center = None, scale = "major"):
+    def M7(self,degree=0,root=0):
+        return self.chord(degree,root,(0,4,7,10))
+
+    def M7M(self,degree=0,root=0):
+        return self.chord(degree, root, (0, 4, 7, 11))
+
+    def m(self,degree = 0,root=0):
+        return self.chord(degree, root, (0, 3, 7))
+
+    def m7(self,degree = 0,root=0):
+        return self.chord(degree, root, (0, 3, 7, 10))
+
+    def m7dim(self,degree = 0,root=0):
+        return self.chord(degree, root, (0, 3, 7, 9))
+
+    def dim7(self,degree = 0,root=0):
+        return self.chord(degree, root, (0, 3, 6, 10))
+
+    def add(self,add=1,tonal_center = None, scale = "major"):
         mainnote_octave = self.name[-1]
         mainnote_set = self.name.replace(str(mainnote_octave), "")
         if tonal_center is None:
             tonal_center = mainnote_set
 
         if scale == "major":
-            difference = (self.add(120)-Note(tonal_center+"0"))%12
+            difference = (self.add1(120)-Note(tonal_center+"0"))%12
             if difference == 0:
-                return self.major_scale(degree = add)
+                return self.major(degree = add)
             elif difference == 2:
-                return self.dorian_scale(degree=add)
+                return self.dorian(degree=add)
             elif difference == 4:
-                return self.phrygian_scale(degree = add)
+                return self.phrygian(degree = add)
             elif difference == 5:
-                return self.lydian_scale(degree = add)
+                return self.lydian(degree = add)
             elif difference == 7:
-                return self.mixolydian_scale(degree = add)
+                return self.mixolydian(degree = add)
             elif difference == 9:
-                return self.minor_scale(degree = add, mode="natural")
+                return self.minor(degree = add, mode="natural")
             elif difference == 11:
-                return self.locrian_scale(degree =add)
+                return self.locrian(degree =add)
         elif scale == "natural minor":
-            difference = (self.add(120) - Note(tonal_center+"0")) % 12
+            difference = (self.add1(120) - Note(tonal_center+"0")) % 12
             if difference == 0:
-                return self.minor_scale(degree=add,mode=scale.split()[0])
+                return self.minor(degree=add,mode=scale.split()[0])
             elif difference == 2:
-                return self.locrian_scale(degree=add,mode = scale.split()[0])
+                return self.locrian(degree=add,mode = scale.split()[0])
             elif difference == 3:
-                return self.ionian_scale(degree=add,mode = scale.split()[0])
+                return self.ionian(degree=add,mode = scale.split()[0])
             elif difference == 5:
-                return self.dorian_scale(degree=add,mode = scale.split()[0])
+                return self.dorian(degree=add,mode = scale.split()[0])
             elif difference == 7:
-                return self.phrygian_scale(degree=add,mode = scale.split()[0])
+                return self.phrygian(degree=add,mode = scale.split()[0])
             elif difference == 8:
-                return self.lydian_scale(degree=add,mode = scale.split()[0])
+                return self.lydian(degree=add,mode = scale.split()[0])
             elif difference == 10:
-                return self.mixolydian_scale(degree=add,mode = scale.split()[0])
+                return self.mixolydian(degree=add,mode = scale.split()[0])
 
         elif scale == "harmonic minor":
-            difference = (self.add(120) - Note(tonal_center+"0")) % 12
+            difference = (self.add1(120) - Note(tonal_center+"0")) % 12
             if difference == 0:
-                return self.minor_scale(degree=add,mode=scale.split()[0])
+                return self.minor(degree=add,mode=scale.split()[0])
             elif difference == 2:
-                return self.locrian_scale(degree=add,mode = scale.split()[0])
+                return self.locrian(degree=add,mode = scale.split()[0])
             elif difference == 3:
-                return self.ionian_scale(degree=add,mode = scale.split()[0])
+                return self.ionian(degree=add,mode = scale.split()[0])
             elif difference == 5:
-                return self.dorian_scale(degree=add,mode = scale.split()[0])
+                return self.dorian(degree=add,mode = scale.split()[0])
             elif difference == 7:
-                return self.phrygian_scale(degree=add,mode = scale.split()[0])
+                return self.phrygian(degree=add,mode = scale.split()[0])
             elif difference == 8:
-                return self.lydian_scale(degree=add,mode = scale.split()[0])
+                return self.lydian(degree=add,mode = scale.split()[0])
             elif difference == 11:
-                return self.mixolydian_scale(degree=add,mode = scale.split()[0])
+                return self.mixolydian(degree=add,mode = scale.split()[0])
 
         elif scale == "melodic minor":
-            difference = (self.add(120) - Note(tonal_center+"0")) % 12
+            difference = (self.add1(120) - Note(tonal_center+"0")) % 12
             if difference == 0:
-                return self.minor_scale(degree=add, mode=scale.split()[0])
+                return self.minor(degree=add, mode=scale.split()[0])
             elif difference == 2:
-                return self.locrian_scale(degree=add, mode=scale.split()[0])
+                return self.locrian(degree=add, mode=scale.split()[0])
             elif difference == 3:
-                return self.ionian_scale(degree=add, mode=scale.split()[0])
+                return self.ionian(degree=add, mode=scale.split()[0])
             elif difference == 5:
-                return self.dorian_scale(degree=add, mode=scale.split()[0])
+                return self.dorian(degree=add, mode=scale.split()[0])
             elif difference == 7:
-                return self.phrygian_scale(degree=add, mode=scale.split()[0])
+                return self.phrygian(degree=add, mode=scale.split()[0])
             elif difference == 9:
-                return self.lydian_scale(degree=add, mode=scale.split()[0])
+                return self.lydian(degree=add, mode=scale.split()[0])
             elif difference == 11:
-                return self.mixolydian_scale(degree=add, mode=scale.split()[0])
+                return self.mixolydian(degree=add, mode=scale.split()[0])
 
     def play(self,tempo = 120, sample_rate=44100):
         time = np.arange(0,(self.duration*60/tempo),1/sample_rate)
@@ -289,34 +350,34 @@ class Note:
         sd.wait()
 
 
-my_timbre = [1,0.5,0.1,0.1,0.1,0.3,0.05]
-note1 = Note("Bb3",dynamic = 1,timbre=my_timbre)
 
+my_timbre = [1,1,2,0.1,0.5,0.1,0.3,0.05]
+note1 = Note("Bb3",dynamic = 1,duration=1,timbre=my_timbre)
 
-#tune of tuba mirum - requiem by mozart down there
+tune_of_tuba_mirum = [3*note1,
+                      3*note1.dominant(),
+                      note1+4,note1,
+                      note1.M(-1),
+                      note1.M(-2),
+                      3*note1-12,
+                      3*note1.change_dynamic(0),
+                      note1,
+                      note1.M(1),
+                      note1.M(2),
+                      note1.M7(3),
+                      note1.M7(2),
+                      note1.M(1),
+                      note1.M7(3)-12,
+                      note1.subdominant(which=1).M(-2),
+                      note1,
+                      note1.subdominant(which=1),
+                      note1.dominant(which=1)+1,
+                      note1.dominant(which=1),
+                      note1.supertonic(),
+                      note1.dominant(which=1),
+                      note1.subdominant(which=1),
+                      3*note1.M(1)]
 
-note1.change_duration(3).play()
-note1.change_duration(3).add(-5).play()
-note1.add(4).play()
-note1.play()
-note1.add(-5).play()
-note1.add(-8).play()
-note1.add(-12).change_duration(3).play()
-note1.change_dynamic(0).change_duration(3).play()
-
-note1.play()
-note1.major(1).play()
-note1.major(2).play()
-note1.add(10).play()
-note1.major(2).play()
-note1.major(1).play()
-note1.add(-2).play()
-note1.add(-3).play()
-note1.play()
-note1.subdominant(which=1).play()
-note1.add(8).play()
-note1.dominant(which=1).play()
-note1.supertonic().play()
-note1.dominant(which=1).play()
-note1.subdominant(which=1).play()
-note1.major(1).change_duration(3).play()
+for i in tune_of_tuba_mirum:
+    print(i.name)
+    i.play()
