174ba21a98ddaaaf753cef31fea9429f48ea442a
artmyb <74744060+artmyb@users.noreply.github.com>
2023-11-28 23:49:42 +0300

    bach.py

--- bach.py
+++ bach.py
@@ -17,11 +17,22 @@ class Note:
             elif isinstance(index, int):
                 return super().__getitem__(index % len(self))
 
-    sets = CircularList(("C","C#","D","Eb","E","F","F#","G","G#","A","Bb","B"))
+    sets = CircularList(("C","C#","D","Eb","E","F","F#","G","Ab","A","Bb","B"))
     fifths = CircularList(("C","G","D","A","E","B","F#","C#","Ab","Eb","Bb","F"))
     scales = {"major":(0,2,4,5,7,9,11,12),
               "harmonic minor":(0, 2, 3, 5, 7, 8, 11, 12),
               "melodic minor":(0, 2, 3, 5, 7, 9, 11, 12)}
+    chords = {"Maj":(0,4,7),
+              "m": (0, 3, 7),
+              "Maj7": (0, 4, 7, 10),
+              "m7": (0, 3, 7, 10),
+              "mMaj7": (0, 3, 7, 11),
+              "7": (0,4,7,10),
+              "dim": (0,3,6),
+              "aug":(0,4,8),
+              "maug7":(0,3,7,11),
+              "dim7": (0,3,6,10),
+              "mM7":(0,3,7,9)}
 
 
     @staticmethod
@@ -45,17 +56,29 @@ class Note:
         self.duration = duration
         self.dynamic = dynamic
         self.timbre = timbre
+        self.name1 = self.name
+        if "Db" in self.name:
+            self.name1 = self.name1.replace("Db","C#")
+        if "D#" in self.name:
+            self.name1 = self.name1.replace("D#","Eb")
+        if "Gb" in self.name:
+            self.name1 = self.name1.replace("Gb","F#")
+        if "G#" in self.name:
+            self.name1 = self.name1.replace("G#", "Ab")
+        if "A#" in self.name:
+            self.name1 = self.name1.replace("A#","Bb")
+
 
     def set(self):
         #returns the name of the note without octave indicator (  A4 = Note("A4), A4.set() = "A" )
         set = ""
-        for i in self.name:
+        for i in self.name1:
             if i.isalpha() or i == "#" or i == "b":
                 set = set + i
         return set
 
     def octave(self):
-        octave = self.name.replace(self.set(),"")
+        octave = self.name1.replace(self.set(),"")
         if "#" in octave:
             octave = octave.replace("#","")
         if "b" in octave:
@@ -76,17 +99,23 @@ class Note:
         return 440*2**((self.key()-69)/12)
 
     def add1(self,semitone=1):
+        if type(semitone) != int:
+            raise Exception("'semitone' parameter must be an integer.")
         return Note.bykey(self.key()+semitone)
 
     def __add__(self,semitone):
         #adding an integer and a note returns a note that is that integerx(semi-tone) above the original note.
+        if type(semitone) != int:
+            raise Exception("'semitone' parameter must be an integer.")
         return self.add1(semitone)
 
     def __radd__(self,semitone):
+        if type(semitone) != int:
+            raise Exception("'semitone' parameter must be an integer.")
         return self.add1(semitone)
 
     def change_duration(self,new_duration):
-        return Note(self.name, duration=new_duration, dynamic=self.dynamic,
+        return Note(self.name1, duration=new_duration, dynamic=self.dynamic,
                     timbre=self.timbre)
 
     def __sub__(self, other):
@@ -96,27 +125,39 @@ class Note:
             return self.key() - other.key()
         elif type(other) == int:
             return self.add1(-other)
+        else:
+            raise Exception("Subtraction is only allowed between Note object & Note object or Note object & integer.")
 
     def __mul__(self, relative_duration):
         #multiplying the note by a number multiplies the duration of it
+        if (type(relative_duration) != int or type(relative_duration) != float) or (relative_duration == 0 or relative_duration < 0):
+            raise Exception("'relative_duration' parameter must be positive number.")
         new_duration = self.duration * relative_duration
         return self.change_duration(new_duration)
 
     def __rmul__(self, relative_duration):
+        if (type(relative_duration) != int or type(relative_duration) != float) or (relative_duration == 0 or relative_duration < 0):
+            raise Exception("'relative_duration' parameter must be positive number.")
         new_duration = self.duration * relative_duration
         return self.change_duration(new_duration)
 
     def change_dynamic(self,new_dynamic):
-        return Note(self.name, duration=self.duration, dynamic=new_dynamic,
+        return Note(self.name1, duration=self.duration, dynamic=new_dynamic,
                     timbre=self.timbre)
 
     def dominant(self,which = 0):
+        if type(which) != int:
+            raise Exception("'which' parameter must be integer.")
         return self.add1(-5+which*12)
 
     def subdominant(self,which = 0):
+        if type(which) != int:
+            raise Exception("'which' parameter must be integer.")
         return self.add1(-7 + which * 12)
 
     def supertonic(self,which = 0):
+        if type(which) != int:
+            raise Exception("'which' parameter must be integer.")
         return self.add1(+2 + which * 12)
 
     def leading_tone(self):
@@ -128,9 +169,35 @@ class Note:
         return self.add1(round(key_difference))
 
     def subharmonic(self,n):
+        if type(n) != int:
+            raise Exception("'n' parameter must be integer.")
         return self.harmonic(1/n)
 
     def chord(self,degree=0,root=0,intervals = (0,4,7)):
+        if "Db" in root:
+            root = root.replace("Db", "C#")
+        if "D#" in root:
+            root = root.replace("D#", "Eb")
+        if "Gb" in root:
+            root = root.replace("Gb", "F#")
+        if "G#" in root:
+            root = root.replace("G#", "Ab")
+        if "A#" in root:
+            root = root.replace("A#", "Bb")
+        if type(degree) != int:
+            raise Exception("'degree' parameter must be integer.")
+        try:
+            temp = Note("root")
+        except:
+            raise Exception("'root' parameter is invalid, must be a note name. (e.g.: C5, Ab4)")
+        if type(intervals) != tuple:
+            raise Exception("'intervals' parameter must be a tuple.")
+        if len(intervals) < 3:
+            raise Exception("'intervals' parameter must have length of at least 3.")
+        for i in intervals:
+            if type(i) != int:
+                Exception("'intervals' parameter must be a tuple of integers types.")
+
         #returns notes of a chord starting from the input note (inversions are included as well)
         #degree is an integer, number uf steps. 0 returns self.
         #root is the root of the note. the input note (self) is the root as default
@@ -157,45 +224,65 @@ class Note:
             root_note= self-difference
             is_there = 0
             for i in range(12):
-                if root_note.chord(i,intervals = intervals).name == self.name:
+                if root_note.chord(i,intervals = intervals).name == self.name1:
                     is_there += 1
                     difference = i
                     break
             if is_there == 0:
                 raise Exception("Note object is not in root note's chord.")
                 return
-            return root_note.chord(degree = degree+difference,intervals = intervals)
+            return root_note.chord(degree =degree+difference,intervals = intervals)
 
-    def M(self, degree=0, root=0): #major chord
-        return self.chord(degree, root, (0, 4, 7))
+    def Maj(self, degree=0, root=0): #major chord
+        return self.chord(degree, root, Note.chords["Maj"])
 
-    def M7(self,degree=0,root=0): # dominant major seventh chord
-        return self.chord(degree,root,(0,4,7,10))
+    def d7(self,degree=0,root=0): # dominant major seventh chord
+        return self.chord(degree,root,Note.chords["7"])
 
-    def M7M(self,degree=0,root=0): #major seventh chord
-        return self.chord(degree, root, (0, 4, 7, 11))
+    def M7(self,degree=0,root=0): #major seventh chord
+        return self.chord(degree, root, Note.chords["M7"])
 
     def m(self,degree = 0,root=0): #minor chord
-        return self.chord(degree, root, (0, 3, 7))
+        return self.chord(degree, root, Note.chords["m"])
 
     def m7(self,degree = 0,root=0): #minor seventh chord
-        return self.chord(degree, root, (0, 3, 7, 10))
+        return self.chord(degree, root, Note.chords["m7"])
 
     def m7dim(self,degree = 0,root=0): #diminished minor seventh chord
-        return self.chord(degree, root, (0, 3, 7, 9))
+        return self.chord(degree, root, Note.chords["m7dim"])
 
-    def m7aug(self, degree=0, root=0): #augmented minor seventh chord
-        return self.chord(degree, root, (0, 3, 7, 11))
+    def maug7(self, degree=0, root=0): #augmented minor seventh chord
+        return self.chord(degree, root, Note.chords["maug7"])
+
+    def dim(self,degree = 0,root=0): #diminished seventh chord
+        return self.chord(degree, root, Note.chords["dim"])
 
     def dim7(self,degree = 0,root=0): #diminished seventh chord
-        return self.chord(degree, root, (0, 3, 6, 10))
+        return self.chord(degree, root, Note.chords["dim7"])
 
     def add(self,add=1,tone = None, scale = "major"):
+        if tone == "Db":
+            tone = "C#"
+        if tone == "D#":
+            tone =="Eb"
+        if tone == "Gb":
+            tone = "F#"
+        if tone == "G#":
+            tone = "Ab"
+        if tone == "A#":
+            tone = "Bb"
+        if type(add) != int:
+            raise Exception("'add' parameter must be integer.")
+        if scale not in list(Note.scales.keys()):
+            str = ""
+            for i in list(Note.scales.keys()):
+                str += ", "+i
+            raise Exception("'scale' parameter must be one of the following:",str)
         # returns a shift of the input note within a scale.
         # default scale is the major scale of the input note.
         # tone parameter must be a note name without octave indicator. ("C", "Ab",...)
         mainnote_octave = self.octave()
-        mainset = self.name.replace(str(mainnote_octave), "")
+        mainset = self.name1.replace(str(mainnote_octave), "")
         if tone is None:
             tone = mainset
             root = 0
@@ -223,7 +310,7 @@ class Note:
             return self.chord(degree=add, root=root, intervals=scale)
 
     def display(self):
-        print("Name:",self.name,",Duration:",self.duration,",MIDI number:",self.key(),",Note set:",self.set())
+        print("Name:",self.name1,",Duration:",self.duration,",MIDI number:",self.key(),",Note set:",self.set())
 
     def play(self,tempo = 120, sample_rate=44100):
         #obvious...
@@ -276,7 +363,6 @@ class Note:
         def display(self):
             for i in self:
                 i.display()
-            return
 
         def __add__(self,other):
             #adding an integer will transpose all the notes.
@@ -336,3 +422,52 @@ class Note:
                     indexes = [item for item, x in enumerate(tone_values) if x == 1]
                     result = result+[(Note.sets[note],scale) for note in indexes]
             return result
+
+        def root(self,aslist=False): #returns the chords to which the notes belong, as string
+            # aslist = False return conventional chord names like CM7
+            # aslist = True returns pairs whose first index is the root note (string), the second index is chord type
+            result = []
+            for chord in list(Note.chords.keys()):
+                tone_values = 12 * [1]
+                for i in self:
+                    belongs = [(i - j).note_index() for j in Note.chords[chord]]
+                    for k in range(len(tone_values)):
+                        if k not in belongs:
+                            tone_values[k] = 0
+                if 1 in tone_values:
+                    indexes = [item for item, x in enumerate(tone_values) if x == 1]
+                    if aslist == True:
+                        result = result + [(Note.sets[note], chord) for note in indexes]
+                    else:
+                        result = result + [(Note.sets[note]+ chord) for note in indexes]
+            return result
+
+        def consonance(self,n=1): #gives consonance value between multiple notes as an integer
+            #the number on its own might not be mean anything.
+            #calculate a base consonance with the note itself to compare and get a relevant result.
+            """
+            the algorithm is a calculation of how well the notes fit together within any harmonic series.
+
+            according to this algorithm, the rank of consonance within an octave;
+                with the upper note held constant:
+                unison, octave, perfect fifth, perfect fourth, major sixth, major third, minor third, minor seventh, tritone, minor sixth, major second, major seventh, minor second
+                with the lower note held constant:
+                octave, unison, perfect fifth, major sixth, perfect fourth, minor seventh, major third, minor sixth, tritone, minor third, major seventh, major second, minor second
+            """
+            keys = [i.key() for i in self]
+            sep = max(keys) - min(keys)
+            print(sep)
+            subharmonics = [[j.subharmonic(i).key() for i in range(1,sep+20+1)] for j in self]
+            for i in range(sep+20):
+                for j in range(1,len(subharmonics)):
+                    current = subharmonics[0][i]
+                    for k in range(sep+20):
+                        found = True
+                        if subharmonics[j][k] != current:
+                            found = False
+                        if found == True:
+                            return current
+
+
+
+
